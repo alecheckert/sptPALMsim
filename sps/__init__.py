8744554bb6e972a325095307ee6a4cf2db28f495
Alec Heckert <alecheckert@gmail.com>
2022-01-19 08:33:10 -0800

    added FBME to __init__.py

--- sps/__init__.py
+++ sps/__init__.py
@@ -1,5 +1,5 @@
 from .camera import Camera
-from .constants import MOTION_TYPE_BROWNIAN
+from .constants import MOTION_TYPE_BROWNIAN, MOTION_TYPE_FRACTIONAL_BROWNIAN
 from .mc import MarkovChain
 from .motion import Motion, BrownianMotion, make_motion
 from .optics import Optics
